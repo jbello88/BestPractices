94a8bbbd21f736a02306b568ded589e0983620df
aa <aa@aa.com>
2020-08-27 14:56:26 +0200

    comments fixed

--- db/models/comment.model.js
+++ db/models/comment.model.js
@@ -5,9 +5,6 @@ const CommentSchema = new Schema(
   {
     content: { type: String, min: 8, max: 1000, required: true },
     by: { type: String, required: true },
-    comments: {
-      type: [CommentSchema],
-    },
   },
   {
     timestamps: true,
